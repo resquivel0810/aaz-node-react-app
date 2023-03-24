1af5e6d1e5587aaa5ab904e3e2d382da9bc00bfd
resquivel0810 <resquivel0810@gmail.com>
2023-03-24 14:31:19 -0600

    Service worker update, first try

--- client/public/worker.js
+++ client/public/worker.js
@@ -46,3 +46,11 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+// This allows the web app to trigger skipWaiting via
+// registration.waiting.postMessage({type: 'SKIP_WAITING'})
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
--- client/src/index.js
+++ client/src/index.js
@@ -28,4 +28,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: (registration) => {
+    registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+    window.location.reload()
+  },
+})
